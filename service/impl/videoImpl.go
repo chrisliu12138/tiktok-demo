9a589df20b4ec9fb0fd8bbed797acb0507723412
limingjie <1607040759@qq.com>
2023-02-07 20:05:56 +0800

    主要在videoImpl.go中增加了 QueryListByVedionl 增加video_test.go 修改了Feed和Publish相关的三个router

--- service/impl/videoImpl.go
+++ service/impl/videoImpl.go
@@ -85,17 +85,11 @@ func Add(uerId uint, playUrl string, title string) bool {
 
 // 根据userID查询稿件
 func Query(userId uint) []Result {
-
-	//连接数据库
-	db, err := gorm.Open(mysql.Open("root:root123@tcp(1.117.88.168:3306)/tiktok?charset=utf8mb4&parseTime=True&loc=Local"))
-	if err != nil {
-		panic("failed to connect database")
-	}
 	//查询某个用户的所有视频
 	//rows := make([]*Result, 0)
 	var rows []Result
 	// SELECT * FROM `video` left join user on user.id = video.user_id where user_id = usrId;
-	result := db.Model(&Video{}).Joins("left join user on user.id = video.user_id").Where("user_id like", userId).Scan(&rows)
+	result := Utils.DB.Model(&Video{}).Joins("left join user on user.id = video.user_id").Where("user_id like", userId).Scan(&rows)
 	if result.Error != nil {
 		return nil //查询失败
 	}
@@ -108,8 +102,6 @@ func Query(userId uint) []Result {
 
 // 根据videoArray查询稿件
 func QueryListByVedionl(videoArray []int64) []Result {
-
-	//查询videoArray为ID的稿件
 	//rows := make([]*Result, 0)
 	var rows []Result
 	// SELECT * FROM `video` left join user on user.id = video.user_id where video.id = ID;
@@ -125,16 +117,11 @@ func QueryListByVedionl(videoArray []int64) []Result {
 
 // 查询最新的30个稿件
 func QueryAll() []Result {
-	//连接数据库
-	db, err := gorm.Open(mysql.Open("root:root@tcp(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=True&loc=Local"))
-	if err != nil {
-		panic("failed to connect database")
-	}
 	//查询最新的30条数据
 	//rows := make([]*Result, 0)
 	var rows []Result
 	// SELECT * FROM `video` left join user on user.id = video.user_id ORDER BY create_time desc  LIMIT 30;
-	result := db.Model(&Video{}).
+	result := Utils.DB.Model(&Video{}).
 		Select("video.id,title,play_url,cover_url,favorite_count,comment_count,is_favorite,video.create_time,user_id,name,follow_count,follower_count,bool").Joins("left join user on user.id = video.user_id").Order("video.create_time desc").Limit(30).Scan(&rows)
 	if result.Error != nil {
 		return nil //查询失败
